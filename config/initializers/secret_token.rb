4571f342b4a9f4ba5882387844b5161278793ae0
Eric Meyer <emeyer@8thlight.com>
2011-02-13 16:25:30 -0600

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ziols::Application.config.secret_token = '9bdf7d7d712c74afb974522e219731f7aaa7a950f81751eb13e4af0d65f6d78b6338bad3018cab50f0ca91a89d4ea45889925222429120ae2257b3e5c2a2bda8'
