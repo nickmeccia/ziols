3a0fadf0f6a8b7134d83d074c4d225f570c8229f
Nick Meccia <Nick@Lappy.local>
2011-03-23 19:37:20 -0500

    updated email

--- app/mailers/ziols_mailer.rb
+++ app/mailers/ziols_mailer.rb
@@ -2,7 +2,6 @@ class ZiolsMailer < ActionMailer::Base
   default :from => "gordonjziols@gmail.com"
   
   def contact_us
-    mail(:to => "nickmeccia@gmail.com",
-             :subject => "Ziols Works!")
+    mail(:to => "nickmeccia@gmail.com", :subject => "Ziols Works", "hello")
   end
 end
