e404d2de727f6b31c56d43447def6fc42786ec3b
Nick Meccia <Nick@Lappy.local>
2011-04-10 16:01:27 -0500

    added jquery stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+
+  $('.showable').hide();
+
+  $('.toggle').click(function() {
+	var element = ("#showable-" + $(this).data("target"));
+    $(element).toggle('normal');
+    return false;
+  });
+
+});
+
